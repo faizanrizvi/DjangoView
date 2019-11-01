8ed33f4985a471d0707c775c3f1c8300398083b2
Faizan Rizvi <rizvi.faizan58@gmail.com>
2019-11-01 14:39:43 +0530

    HttpResponse & HttpResponseRedirect

--- src/djviews/views.py
+++ src/djviews/views.py
@@ -1,13 +1,23 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 
 # Class Home
 
+# def home(request):
+#     #print(request)
+#     #print(dir(request))
+#     #print(request.method)
+#     #print(request.is_ajax)
+#     #print(request.is_ajax())
+#     #print(request.get_full_path())
+#     return HttpResponse("<!DOCTYPE html><html><head><style>h1{color: red;}</style></head><body><h1>Hello World</h1></body></html>")
+
 def home(request):
-    #print(request)
-    print(dir(request))
-    #print(request.method)
-    #print(request.is_ajax)
-    #print(request.is_ajax())
-    print(request.get_full_path())
-    return HttpResponse("<!DOCTYPE html><html><head><style>h1{color: red;}</style></head><body><h1>Hello World</h1></body></html>")
+	response = HttpResponse(content_type='text/html')
+	#response = HttpResponse(content_type='application/json')
+	response.content = "<!DOCTYPE html><html><head><style>h1{color: red;}</style></head><body><h1>Hello World</h1></body></html>"
+	response.write("<p>Here's the text of the Web page.</p>")
+	return response
+
+def redirect_somewhere(request):
+    return HttpResponseRedirect("/some/path")
