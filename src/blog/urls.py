5db39e2230d9cf3d4f4e2d32c3458ac948c02c29
Faizan Rizvi <rizvi.faizan58@gmail.com>
2019-11-03 12:14:31 +0530

    login required

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url,include
-from .views import  post_list_view
+from .views import  post_list_view,login_required_view
 
 
 urlpatterns = [
     
-    url(r'^$', post_list_view,name='post_list')
+    url(r'^$', post_list_view,name='post_list'),
+    url(r'^public$', login_required_view,name='login_required')
+
 
     ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import  HttpResponse
+from django.http import  HttpResponse, Http404, HttpResponseRedirect
 from .models import  PostModel
+from django.contrib.auth.decorators import  login_required
 
 # Create your views here.
 
@@ -9,3 +10,19 @@ def post_list_view(request):
 	print(qs)
 	context = {"object_list":qs}
 	return render(request,"list-view.html", context)
+
+#@login_required(login_url = '/login/')
+@login_required
+def login_required_view(request):
+	print(request.user)
+	qs = PostModel.objects.all()
+	print(qs)
+	context = {"object_list":qs}
+	if request.user.is_authenticated():
+		template = 'list-view.html'
+	else:
+		template = 'list-view-public.html'
+		#raise Http404
+		#return HttpResponseRedirect('/login/')
+	return render(request,template,context)
+
