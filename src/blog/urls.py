154adeff699fd3bc03bd91d8903d63a64a86cedf
Faizan Rizvi <rizvi.faizan58@gmail.com>
2019-11-05 00:14:37 +0530

    create and update view

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import url,include
-from .views import  post_list_view,login_required_view,post_detail_view
+from .views import  (post_list_view,
+					login_required_view,
+					post_detail_view,
+					post_create_view,
+					post_update_view)
 
 
 urlpatterns = [
     
     url(r'^$', post_list_view,name='post_list'),
+    url(r'^create$', post_create_view,name='create'),
+    url(r'^(?P<id>\d+)/edit/$', post_update_view, name='update'),
     url(r'^(?P<id>\d+)/$', post_detail_view,name='detail'),
     url(r'^public$', login_required_view,name='login_required')
 
--- src/blog/views.py
+++ src/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,get_object_or_404
 from django.http import  HttpResponse, Http404, HttpResponseRedirect
 from .models import  PostModel
 from django.contrib.auth.decorators import  login_required
+from .forms import  PostModelForm
+from django.contrib import  messages
 
 # Create your views here.
 
@@ -17,10 +19,36 @@ def post_detail_view(request, id=None):
 	context = {"object":obj}
 	return render(request,'detail-view.html', context)
 
+def post_create_view(request):
+	form = PostModelForm(request.POST or None)
+	context = {"form":form}
+	if form.is_valid():
+		obj = form.save(commit=False)
+		#print(obj.title)
+		obj.save()
+		messages.success(request, "Created a new blog post!")
+		context = {
+		    "form": PostModelForm()
+		}
+	return render(request, 'create-view.html',context)
+
+def post_update_view(request, id=None):
+	obj = get_object_or_404(PostModel, id=id)
+	form = PostModelForm(request.POST or None, instance=obj)
+	context = {"form":form}
+	if form.is_valid():
+		obj = form.save(commit=False)
+		#print(obj.title)
+		obj.save()
+		messages.success(request, "Updated Post")
+		return HttpResponseRedirect('/blog/{id}'.format(id=obj.id))
+		
+	return render(request, 'update-view.html',context)
+
 #@login_required(login_url = '/login/')
 @login_required
 def login_required_view(request):
-	print(request.user)
+	print(request.user) 
 	qs = PostModel.objects.all()
 	print(qs)
 	context = {"object_list":qs}
